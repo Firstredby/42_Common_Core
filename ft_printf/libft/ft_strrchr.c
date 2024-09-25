9553e5fe64f016a14c35eb9b06ab99ba649b5218
Firstredby <imshchirov@gmail.com>
2024-09-25 22:45:14 +0200

    include libft library

---
+++ ft_printf/libft/ft_strrchr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishchyro <ishchyro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 14:13:37 by ishchyro          #+#    #+#             */
+/*   Updated: 2024/09/18 17:04:29 by ishchyro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const	char *s, int c)
+{
+	int		i;
+	int		cp;
+	char	*str;
+	int		check;
+
+	i = 0;
+	check = 0;
+	str = (char *)s;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+		{
+			check = 1;
+			cp = i;
+		}
+		i++;
+	}
+	if ((char)c == '\0')
+		return (str + i);
+	if (!check && i == (int)ft_strlen(s))
+		return (NULL);
+	while (cp--)
+		str++;
+	return (str);
+}
