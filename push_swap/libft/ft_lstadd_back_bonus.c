33df078f3c3b1d0a5ff8692bfb236dc51522f0b1
Firstredby <imshchirov@gmail.com>
2025-02-01 20:24:24 +0100

    fixed leaks with only 2 arguments

--- push_swap/libft/ft_lstadd_back_bonus.c
+++ push_swap/libft/ft_lstadd_back_bonus.c
@@ -6,35 +6,48 @@
 /*   By: ishchyro <ishchyro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 15:16:59 by ishchyro          #+#    #+#             */
-/*   Updated: 2024/12/19 15:35:28 by ishchyro         ###   ########.fr       */
+/*   Updated: 2025/01/17 18:04:11 by ishchyro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	emptylist(t_list **lst, t_list *new)
+{
+	if (!new || !lst)
+	{
+		ft_lstclear(*lst);
+		free(*lst);
+		*lst = NULL;
+		return (0);
+	}
+	return (1);
+}
+
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*head;
 
-	if (!new || !lst)
-		return (ft_lstclear(*lst));
-	if (!*lst)
-		*lst = new;
-	else
+	if (emptylist(lst, new))
 	{
-		head = *lst;
-		if (head->next)
-		{
-			head = head->prev;
-			new->next = *lst;
-			(*lst)->prev = new;
-		}
+		if (!*lst)
+			*lst = new;
 		else
 		{
-			head->prev = new;
-			new->next = head;
+			head = *lst;
+			if (head->next)
+			{
+				head = head->prev;
+				new->next = *lst;
+				(*lst)->prev = new;
+			}
+			else
+			{
+				head->prev = new;
+				new->next = head;
+			}
+			head->next = new;
+			new->prev = head;
 		}
-		head->next = new;
-		new->prev = head;
 	}
 }
