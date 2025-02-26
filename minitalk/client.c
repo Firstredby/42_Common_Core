e49acbc2ad8bb154dcc1db6890b5dd5aaf3b66ea
Firstredby <imshchirov@gmail.com>
2025-02-26 15:50:04 +0100

    now client side protected from invalid PID

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: ishchyro <ishchyro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 04:23:21 by ishchyro          #+#    #+#             */
-/*   Updated: 2025/02/02 15:15:53 by ishchyro         ###   ########.fr       */
+/*   Updated: 2025/02/26 15:47:56 by ishchyro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,11 +67,11 @@ int	main(int ac, char **av)
 		ft_printf("Wrong input!\n");
 		ft_printf("Please note that first paramether should ");
 		ft_printf("be server id, then a message!");
-		exit(-1);
+		exit(1);
 	}
 	if (ft_atoi(av[1]) <= 0 || av[1][0] == '+'
 		|| ft_atoi(av[1]) > 419304)
-		ft_printf("Invalid PID!");
+		return (ft_printf("Invalid PID!"));
 	signal(SIGUSR1, &respond);
 	signal(SIGUSR2, &respond);
 	msg(ft_atoi(av[1]), av[2]);
