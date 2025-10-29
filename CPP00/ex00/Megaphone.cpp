cd69fba375af178fbbcd7902c5c48550c13a58ca
Firstredby <imshchirov@gmail.com>
2025-10-29 13:42:25 +0100

    ascii check

--- CPP00/ex00/Megaphone.cpp
+++ CPP00/ex00/Megaphone.cpp
@@ -5,9 +5,18 @@ int main(int ac, char **av)
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
+	{
+		for (int i = 1; av[i]; i++)
+			for (int j = 0; av[i][j]; j++)
+				if (av[i][j] > 127)
+				{
+					std::cout << "Why tf you use unicode man?" << std::endl;
+					return 1;
+				}
 		for (int i = 1; av[i]; i++)
 			for (int j = 0; av[i][j]; j++)
 				std::cout << (char)toupper(av[i][j]);
+	}
 	std::cout << std::endl;
 	return 0;
 }
