deb1a0f6feb146445dd4a57a41d7df34218dcf48
Firstredby <imshchirov@gmail.com>
2025-11-14 14:44:49 +0100

    Changed logic of invoking functions

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -8,54 +8,34 @@ Harl::~Harl()
 {
 }
 
-void	Harl::debug(void)
+void	Harl::Debug(void)
 {
 	cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << endl;
 }
-void	Harl::info(void)
+void	Harl::Info(void)
 {
 	cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << endl;
 }
-void	Harl::warning(void)
+void	Harl::Warning(void)
 {
 	cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << endl;
 }
-void	Harl::error(void)
+void	Harl::Error(void)
 {
 	cout << "This is unacceptable! I want to speak to the manager now." << endl;
 }
 
-elevel	strToEnum(std::string level)
+void	Harl::Undefined(void)
 {
-	static std::map<std::string, elevel> table;
-	table["INFO"] = INFO; 
-	table["DEBUG"] = DEBUG;
-	table["WARNING"] = WARNING;
-	table["ERROR"] = ERROR;
-	std::map<std::string, elevel>::const_iterator it = table.find(level);
-	if(it != table.end())
-		return it->second;
-	return UNKNOWN;
+	cout << "I don't know what you want from me! >:(" << endl;
 }
 
 void	Harl::complain(std::string level)
 {
-	switch (strToEnum(level))
-	{
-		case DEBUG:
-			debug();
-			break;
-		case INFO:
-			info();
-			break;
-		case WARNING:
-			warning();
-			break;
-		case ERROR:
-			error();
-			break;
-		default:
-			cout << "I don't understand what you want from me >:(" << endl;
-			break;
-	}
+	static std::string table[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+	func a[] = { &Harl::Debug, &Harl::Info, &Harl::Warning, &Harl::Error, &Harl::Undefined };
+	size_t res = 0;
+	while (!table[res].empty() && table[res].compare(level))
+		res++;
+	(this->*a[res])();
 }
